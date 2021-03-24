8b1368720fad1ab6faa05e7ca577f6f40598e4ac
kharkov-timur <kharkov.timur@gmail.com>
2021-03-24 17:46:43 +0200

    DATABASE_URL

--- db.js
+++ db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize')
 
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  // process.env.DB_NAME, // name database
-  // process.env.DB_USER, // user database
-  // process.env.DB_PASSWORD, // user password
+  // process.env.DATABASE_URL,
+  process.env.DB_NAME, // name database
+  process.env.DB_USER, // user database
+  process.env.DB_PASSWORD, // user password
   {
     dialect: 'postgres',
     host: process.env.DB_HOST,
