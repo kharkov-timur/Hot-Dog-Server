ee91e5453ef4680e9ad8913630e11920fb882f18
kharkov-timur <kharkov.timur@gmail.com>
2021-03-25 07:48:24 +0200

    DATABASE_URL

--- db.js
+++ db.js
@@ -1,33 +1,33 @@
-const pg = require('pg')
-pg.defaults.ssl = true
-
+// const pg = require('pg')
+// pg.defaults.ssl = true
+//
 const { Sequelize } = require('sequelize')
 
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME, // name database
-//   process.env.DB_USER, // user database
-//   process.env.DB_PASSWORD, // user password
-//   {
-//     dialect: 'postgres',
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT
-//   }
-// )
-
 const sequelize = new Sequelize(
-  process.env.DATABASE_URL,
-  // process.env.DB_NAME, // name database
-  // process.env.DB_USER, // user database
-  // process.env.DB_PASSWORD, // user password
+  process.env.DB_NAME, // name database
+  process.env.DB_USER, // user database
+  process.env.DB_PASSWORD, // user password
   {
     dialect: 'postgres',
-    dialectOptions: {
-      "ssl": true
-    },
-    host: 'localhost',
-    port: 5432,
-    ssl: true
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT
   }
 )
+
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_URL,
+//   // process.env.DB_NAME, // name database
+//   // process.env.DB_USER, // user database
+//   // process.env.DB_PASSWORD, // user password
+//   {
+//     dialect: 'postgres',
+//     dialectOptions: {
+//       "ssl": true
+//     },
+//     host: 'localhost',
+//     port: 5432,
+//     ssl: true
+//   }
+// )
 
 module.exports = sequelize
